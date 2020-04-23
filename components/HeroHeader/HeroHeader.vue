12705cfc458d07fd821e78ae84277cbdc910e184
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-04-23 11:36:43 +0200

    refactored website

---
+++ components/HeroHeader/HeroHeader.vue
@@ -0,0 +1,127 @@
+<template>
+  <b-jumbotron
+    style="min-height: 70vh; background: linear-gradient(to bottom right, #ff9494 0%, #ee64f6 100%); margin-top: 5vh"
+    class="mb-0"
+  >
+    <b-container>
+      <h1 class="display-3 text-white mb-0">Watch anything. Together.</h1>
+      <p class="text-white">
+        * Runs on Google Chrome. Paid services require every participant's
+        subscription.
+      </p>
+      <SupportedServices />
+    </b-container>
+    <b-container fluid>
+      <b-row>
+        <b-col sm class="d-flex justify-content-center mb-5">
+          <svg
+            id="jellyfish"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 512 512"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <defs>
+              <style>
+                .cls-1 {
+                  fill: #fff;
+                }
+
+                .cls-2 {
+                  fill: url(#linear-gradient);
+                }
+              </style>
+              <linearGradient
+                id="linear-gradient"
+                x1="32"
+                y1="256"
+                x2="480"
+                y2="256"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0" stop-color="#9164ff" />
+                <stop offset="0.725" stop-color="#8bfff4" />
+                <stop offset="1" stop-color="#8bfff4" />
+              </linearGradient>
+            </defs>
+            <title>&deg;&deg;&deg;&deg;</title>
+            <g id="Layer_3" data-name="Layer 3">
+              <path
+                class="cls-1"
+                d="M414.3921,97.6079A224,224,0,0,0,32,256v48a16,16,0,0,0,16,16H464a16,16,0,0,0,16-16V256A222.5338,222.5338,0,0,0,414.3921,97.6079Z"
+              />
+              <path
+                class="cls-2"
+                d="M414.3916,97.6079A224,224,0,0,0,32,256v48a16,16,0,0,0,16,16H96v48a16.0183,16.0183,0,0,1-16,16,16,16,0,0,0,0,32,48.054,48.054,0,0,0,48-48V320h64V432a16.0183,16.0183,0,0,1-16,16,16,16,0,0,0,0,32,48.054,48.054,0,0,0,48-48V320h64V432a48.054,48.054,0,0,0,48,48,16,16,0,0,0,0-32,16.0183,16.0183,0,0,1-16-16V320h64v48a48.054,48.054,0,0,0,48,48,16,16,0,0,0,0-32,16.0183,16.0183,0,0,1-16-16V320h48a16,16,0,0,0,16-16V256A222.5322,222.5322,0,0,0,414.3916,97.6079Zm-228.9,59.273a127.8013,127.8013,0,0,0-28.1279,28.3862,16,16,0,0,1-26.086-18.5342,157.9553,157.9553,0,0,1,35.2315-35.6138,16,16,0,1,1,18.9824,25.7618Z"
+              />
+            </g>
+          </svg>
+        </b-col>
+        <b-col sm class="d-flex align-items-center justify-content-center mb-5">
+          <p class="lead text-white text-center" style="max-width: 20em">
+            Jelly-Party synchronizes videos you play in your browser, so that
+            you and your friends can enjoy your favorite content —
+            <span style="font-weight: 600"> together.</span>
+          </p>
+        </b-col>
+        <b-col sm class="d-flex align-items-center justify-content-center mb-5">
+          <a
+            href="https://chrome.google.com/webstore/detail/jelly-party/aiecbkandfgpphpdilbaaagnampmdgpd"
+            ><button
+              class="custom-button"
+              id="ctaButton"
+              style="--content: 'Get the extension now!';"
+            >
+              <div class="left"></div>
+              Get the extension now!
+              <div class="right"></div></button
+          ></a>
+        </b-col>
+      </b-row>
+    </b-container>
+  </b-jumbotron>
+</template>
+
+<script>
+import gsap from 'gsap'
+import SupportedServices from './SupportedServices.vue'
+
+export default {
+  mounted() {
+    function flickerAnimate(object) {
+      const tl = gsap.timeline()
+      const randomDuration = Math.random() * 0.5 + 1.5
+      const randomRotation =
+        (Math.random() > 0.5 ? '-' : '') + (Math.random() * 10 + 2) + 'deg'
+      const randomX =
+        (Math.random() > 0.5 ? -1 : 1) * Math.random() * 20 * excitement
+      const randomY =
+        (Math.random() > 0.5 ? -1 : 1) * Math.random() * 20 * excitement + 20
+      tl.to(object, {
+        duration: randomDuration / excitement,
+        x: randomX,
+        y: randomY,
+        rotate: randomRotation,
+        onComplete: flickerAnimate,
+        onCompleteParams: [object]
+      })
+    }
+    let excitement = 1
+    const ctaButton = document.getElementById('ctaButton')
+    const jellyfish = document.getElementById('jellyfish')
+    ctaButton.addEventListener('mouseenter', () => {
+      gsap.to(jellyfish, { duration: 0.2, rotation: '360deg' })
+      excitement = 5
+    })
+    ctaButton.addEventListener('mouseleave', () => {
+      excitement = 1
+    })
+    flickerAnimate(jellyfish)
+  },
+  components: {
+    SupportedServices
+  }
+}
+</script>
+
+<style></style>
---
+++ components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-navbar class="fixed-top" style="background-color: #272727">
+    <b-container>
+      <!-- Content here -->
+      <b-navbar-nav>
+        <b-navbar-brand href="https://www.jelly-party.com"
+          ><h2 class="brand-name">Jelly Party</h2></b-navbar-brand
+        >
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <a
+          class="text-decoration-none"
+          href="https://chrome.google.com/webstore/detail/jelly-party/aiecbkandfgpphpdilbaaagnampmdgpd"
+          ><button
+            style="font-size:1em"
+            class="btn btn-primary mr-2 d-none d-sm-block custom-button"
+          >
+            Get the extension
+          </button></a
+        >
+        <a
+          class="text-decoration-none"
+          href="https://www.patreon.com/jellyparty"
+          ><button style="font-size: 1em" class="btn btn-primary custom-button">
+            Support us on Patreon
+          </button></a
+        >
+      </b-navbar-nav>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.brand-name {
+  color: white;
+}
+
+.brand-name:hover {
+  transition: color 0.2s ease-in-out;
+  color: red;
+}
+</style>
